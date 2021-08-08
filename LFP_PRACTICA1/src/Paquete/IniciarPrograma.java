95ec940a6559312fc26d013069deaf8574bb98b7
Josué Casiá <josuecasia201831432@cunoc.edu.gt>
2021-08-07 21:55:29 -0600

    primer commit con git ignore y ventana

---
+++ LFP_PRACTICA1/src/Paquete/IniciarPrograma.java
@@ -0,0 +1,13 @@
+
+package Paquete;
+
+public class IniciarPrograma {
+    
+        public static void main(String[] args) {
+        Ventana nuevaVentana = new Ventana();
+        nuevaVentana.setVisible(true);
+        nuevaVentana.setLocationRelativeTo(null);
+        
+    }
+    
+}
